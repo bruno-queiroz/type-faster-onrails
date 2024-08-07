ec23b597f1319cedd98b3e3cce89d752dcae9e07
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-08-07 08:23:08 -0300

    Add tsChecker

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,24 +1,27 @@
-// The source code including full typescript support is available at: 
+// The source code including full typescript support is available at:
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/development.js
 
-const { devServer, inliningCss } = require('shakapacker');
+const { devServer, inliningCss } = require("shakapacker");
+const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-const webpackConfig = require('./webpackConfig');
+const webpackConfig = require("./webpackConfig");
 
 const developmentEnvOnly = (clientWebpackConfig, _serverWebpackConfig) => {
   // plugins
+  clientWebpackConfig.plugins.push(new ForkTSCheckerWebpackPlugin());
+
   if (inliningCss) {
     // Note, when this is run, we're building the server and client bundles in separate processes.
     // Thus, this plugin is not applied to the server bundle.
 
     // eslint-disable-next-line global-require
-    const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+    const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
     clientWebpackConfig.plugins.push(
       new ReactRefreshWebpackPlugin({
         overlay: {
           sockPort: devServer.port,
         },
-      }),
+      })
     );
   }
 };
