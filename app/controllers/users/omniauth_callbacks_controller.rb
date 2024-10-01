7836a99fd8fa6957c9dc910e46db05566767fc02
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-10-01 08:46:21 -0300

    Add Users::OmniauthCallbacksController for Google OAuth2 authentication

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,21 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  respond_to :html, :json
+
+  def google_oauth2
+        # You need to implement the method below in your model (e.g. app/models/user.rb)
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+       puts "======= test ========="
+       puts session
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+          # render :sign_in, status: :created
+        else
+          session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+          render :sign_in_error, status: :unprocessable_entity
+        end
+  end
+    
+  def failure
+      redirect_to root_path
+  end
+end
