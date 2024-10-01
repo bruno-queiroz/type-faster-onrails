85426b8ab26ae9d5bad46a66ecfa659c87228109
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-10-01 08:26:47 -0300

    Create game modes

---
+++ app/javascript/bundles/content/gameModes.ts
@@ -0,0 +1,12 @@
+export const gameModes = [
+  {
+    mode: "Traditional",
+    description: "Practice typing with random texts of books, movies etc...",
+    slug: "traditional",
+  },
+  {
+    mode: "Repeated Words",
+    description: "Get better at writing specific words, repetition is the key.",
+    slug: "repeated-words",
+  },
+];
