46562454bd7dcc3ebdc79467e0ca7e5dbed55a3b
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-10-01 08:19:02 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "application#index"
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  post "texts" => "texts#create"
+  get "texts" => "texts#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +14,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get '/*spa' => 'application#index'
 end
