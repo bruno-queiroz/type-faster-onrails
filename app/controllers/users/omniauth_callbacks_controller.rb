c34003bbece4bc1a91b39198c2a56ebe9573a6e0
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-10-01 09:08:36 -0300

    Remove puts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         @user = User.from_omniauth(request.env["omniauth.auth"])
-       puts "======= test ========="
-       puts session
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-          # render :sign_in, status: :created
         else
           session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
           render :sign_in_error, status: :unprocessable_entity
